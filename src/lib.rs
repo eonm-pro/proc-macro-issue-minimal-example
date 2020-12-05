d262fefa30daa72e17aa3f61d3229c7d3c87eeda
Mathis EON <eon@abes.fr>
2020-12-05 12:45:58 +0100

    Parsing with ItemStruct

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,21 @@
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, DeriveInput, parse::Parser};
 use quote::quote;
+use syn::{parse::Parser, parse_macro_input, ItemStruct};
 
 #[proc_macro_attribute]
-pub fn add_field(_args: TokenStream, input: TokenStream) -> TokenStream  {
-    let mut ast = parse_macro_input!(input as DeriveInput);
-    match &mut ast.data {
-        syn::Data::Struct(ref mut struct_data) => {           
-            match &mut struct_data.fields {
-                syn::Fields::Named(fields) => {
-                    fields
-                        .named
-                        .push(syn::Field::parse_named.parse2(quote! { pub a: String }).unwrap());
-                }   
-                _ => {
-                    ()
-                }
-            }              
-            
-            // I tried
-            // 
-            return quote! {
-                #ast
-            }.into();
-        }
-        _ => panic!("AddField has to be used with structs "),
+pub fn add_field(_args: TokenStream, input: TokenStream) -> TokenStream {
+    let mut item_struct = parse_macro_input!(input as ItemStruct);
+
+    if let syn::Fields::Named(ref mut fields) = item_struct.fields {
+        fields.named.push(
+            syn::Field::parse_named
+                .parse2(quote! { pub a: String })
+                .unwrap(),
+        );
     }
+
+    return quote! {
+        #item_struct
+    }
+    .into();
 }
--- tests/01-shoud-add-struct-field.rs
+++ tests/01-shoud-add-struct-field.rs
@@ -2,10 +2,8 @@
 mod tests {
     use proc_macro_issue_minimal_example::add_field;
 
-    
     #[test]
     fn add_field() {
-        
         #[add_field]
         #[derive(Debug, Clone)]
         struct Foo {}
@@ -15,8 +13,33 @@ mod tests {
         //  pub a: String
         // }
 
-        let bar = Foo { a: "lorem ipsum".to_string()};
+        let bar = Foo {
+            a: "lorem ipsum".to_string(),
+        };
+
         assert_eq!(bar.a, "lorem ipsum");
     }
 
+    #[test]
+    fn keep_original_field() {
+        #[add_field]
+        #[derive(Debug, Clone)]
+        struct Foo {
+            b: String,
+        }
+
+        // Foo should be expand to :
+        // struct Foo {
+        //  pub a: String
+        //  pub b: String,
+        // }
+
+        let bar = Foo {
+            a: "lorem ipsum".to_string(),
+            b: "dolor sit amet".to_string(),
+        };
+
+        assert_eq!(bar.a, "lorem ipsum");
+        assert_eq!(bar.b, "dolor sit amet");
+    }
 }
