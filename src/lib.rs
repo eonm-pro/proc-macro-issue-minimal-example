45089b7634045ded8b7e136dbf10730bf008e48f
Mathis EON <eon@abes.fr>
2020-12-05 13:21:03 +0100

    Add parse::Nothing

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse::Parser, parse_macro_input, ItemStruct};
+use syn::{parse, parse::Parser, parse_macro_input, ItemStruct};
 
 #[proc_macro_attribute]
-pub fn add_field(_args: TokenStream, input: TokenStream) -> TokenStream {
+pub fn add_field(args: TokenStream, input: TokenStream) -> TokenStream {
     let mut item_struct = parse_macro_input!(input as ItemStruct);
+    let _ = parse_macro_input!(args as parse::Nothing);
 
     if let syn::Fields::Named(ref mut fields) = item_struct.fields {
         fields.named.push(
